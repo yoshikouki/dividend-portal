9a581f2819aab74d01c8127020f0b205cba8c765
yoshikouki <yoshikouki@gmail.com>
2021-06-19 18:22:44 +0900

    scaffold Company

---
+++ db/migrate/20210619092045_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :companies do |t|
+      t.string :symbol
+      t.string :name
+      t.string :exchange
+
+      t.timestamps
+    end
+  end
+end
