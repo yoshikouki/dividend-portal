1eb7f07076c828167ba5942a9f9a33b3c1d4f98f
yoshikouki <yoshikouki@gmail.com>
2021-06-20 12:41:45 +0900

    毎日企業情報を最新情報に更新する

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,19 +2,11 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-
-# Example:
 #
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
+
+set :output, "/path/to/my/cron_log.log"
+
+every 1.day do
+  rake "company:update_to_latest"
+end
---
+++ lib/tasks/company.rb
@@ -0,0 +1,5 @@
+namespace :company do
+  desc "Update to the latest data"
+  task :update_to_latest do
+  end
+end
