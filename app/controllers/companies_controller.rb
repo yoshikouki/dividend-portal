bf42741d1e4f5878af31f8d72ba083cc514caa3a
yoshikouki <yoshikouki@gmail.com>
2021-06-20 11:36:24 +0900

    Company は表示しかしない

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CompaniesController < ApplicationController
-  before_action :set_company, only: %i[show edit update destroy]
+  before_action :set_company, only: %i[show]
 
   # GET /companies or /companies.json
   def index
@@ -12,61 +12,10 @@ class CompaniesController < ApplicationController
   def show
   end
 
-  # GET /companies/new
-  def new
-    @company = Company.new
-  end
-
-  # GET /companies/1/edit
-  def edit
-  end
-
-  # POST /companies or /companies.json
-  def create
-    @company = Company.new(company_params)
-
-    respond_to do |format|
-      if @company.save
-        format.html { redirect_to @company, notice: "Company was successfully created." }
-        format.json { render :show, status: :created, location: @company }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /companies/1 or /companies/1.json
-  def update
-    respond_to do |format|
-      if @company.update(company_params)
-        format.html { redirect_to @company, notice: "Company was successfully updated." }
-        format.json { render :show, status: :ok, location: @company }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /companies/1 or /companies/1.json
-  def destroy
-    @company.destroy
-    respond_to do |format|
-      format.html { redirect_to companies_url, notice: "Company was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_company
     @company = Company.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def company_params
-    params.require(:company).permit(:symbol, :name, :exchange)
   end
 end
