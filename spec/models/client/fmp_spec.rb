426f7090d4302fea4725bf73c2ca40510dd4122f
yoshikouki <yoshikouki@gmail.com>
2021-06-20 12:06:24 +0900

    fix test

--- spec/models/client/fmp_spec.rb
+++ spec/models/client/fmp_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Client::Fmp, type: :model do
   describe "#url" do
     valid_path = "/api/v3/stock/list"
     valid_path2 = "api/v3/stock/list"
-    invalid_path = "//api/v3/stock/list"
 
     it "path should be valid" do
       expect(Client::Fmp.url(valid_path)).to be_truthy
