d40bfd66d4aad9607b0ff49c2b458a0425e5a1ef
yoshikouki <yoshikouki@gmail.com>
2021-06-20 11:39:06 +0900

    FMP から企業一覧を取得して表示させる

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class Client < ApplicationRecord
+  def self.get(url)
+    uri = URI.parse(url)
+    req = Net::HTTP::Get.new(uri)
+    req["Upgrade-Insecure-Requests"] = "1"
+    options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    res = Net::HTTP.start(uri.host, uri.port, options) {|http|
+      http.request(req)
+    }
+
+    if res["content-type"].include?("application/json")
+      JSON.parse res.body
+    else
+      res.body
+    end
+  end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  def self.all
+    Client::Fmp.get_symbols_list
+  end
 end
