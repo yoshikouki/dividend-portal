224633af42496e2ddd99a62234a54e233a892796
yoshikouki <yoshikouki@gmail.com>
2021-06-29 08:52:59 +0900

    Twitter Gem を使った TwitterWrapper を作る

--- app/models/client/twitter.rb
+++ app/models/client/twitter.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 module Client
-  class Twitter < ApplicationRecord
+  module TwitterWrapper
     API_HOST = "api.twitter.com"
     API_KEY = ENV["API_KEY_FMP"]
+
+    def self.new
+      Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"],
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"],
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"],
+        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"],
+      end
+    end
   end
 end
