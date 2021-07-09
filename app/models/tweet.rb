e2367a0778ef9c405c2e8fe0956ec4c946b8aeff
yoshikouki <yoshikouki@gmail.com>
2021-07-09 22:58:02 +0900

    ツイートの責務は全て Tweet が担う

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,7 +6,19 @@ module Tweet
     client.update(text)
   end
 
-  def render_ex_dividend_previous_date(count = nil, symbols_text = nil)
+  def self.ex_dividend_previous_date
+    tomorrow = Time.at(1.day.since).strftime("%Y-%m-%d")
+    dividends = Client::Fmp.get_dividend_calendar(from: tomorrow, to: tomorrow)
+
+    # ツイート文面を作成
+    symbols_text = dividends.map { |dividend| "$#{dividend[:symbol]}" }.sort.join(" ")
+
+    # ツイート
+    tweet_text = render_ex_dividend_previous_date(dividends.count, symbols_text)
+    tweet(tweet_text)
+  end
+
+  def self.render_ex_dividend_previous_date(count = nil, symbols_text = nil)
     <<~TWEET
       今日までの購入で配当金が受け取れる米国株は「#{count}件」です (配当落ち前日)
       #{symbols_text}
--- lib/tasks/tweet_task.rake
+++ lib/tasks/tweet_task.rake
@@ -3,14 +3,6 @@
 namespace :tweet do
   desc "権利落ち前日の米国株を配信する"
   task ex_dividend_previous_date: :environment do
-    tomorrow = Time.at(1.day.since).strftime("%Y-%m-%d")
-    dividends = Client::Fmp.get_dividend_calendar(from: tomorrow, to: tomorrow)
-
-    # ツイート文面を作成
-    symbols_text = dividends.map { |dividend| "$#{dividend[:symbol]}" }.sort.join(" ")
-
-    # ツイート
-    tweet_text = Tweet.render_ex_dividend_previous_date(dividends.count, symbols_text)
-    Tweet.tweet(tweet_text)
+    Tweet.ex_dividend_previous_date
   end
 end
