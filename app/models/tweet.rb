501b9f8f7a73d15701e076f8ab94e9efb6deea0b
yoshikouki <yoshikouki@gmail.com>
2021-07-09 23:47:06 +0900

    シンボルテキストを作るメソッドも責務的にこっちかも

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,16 +10,13 @@ module Tweet
     tomorrow = Time.at(1.day.since)
     dividends = Dividend.filter_by_ex_dividend_date(tomorrow)
 
-    # ツイート文面を作成
-    symbols_text = dividends.map { |dividend| "$#{dividend[:symbol]}" }.sort.join(" ")
-      symbols_text.length
-
-    # ツイート
-    tweet_text = render_ex_dividend_previous_date(dividends.count, symbols_text)
+    tweet_text = render_ex_dividend_previous_date(dividends)
     tweet(tweet_text)
   end
 
-  def self.render_ex_dividend_previous_date(count = nil, symbols_text = nil)
+  def self.render_ex_dividend_previous_date(dividends = [])
+    symbols_text = dividends.map { |dividend| "$#{dividend[:symbol]}" }.sort.join(" ")
+
     <<~TWEET
       今日までの購入で配当金が受け取れる米国株は「#{count}件」です (配当落ち前日)
       #{symbols_text}
