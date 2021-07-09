3f4f263baaebbdc5378116f2d6f3368aa69a3ea7
yoshikouki <yoshikouki@gmail.com>
2021-07-09 22:55:15 +0900

    ツイート内容をTweetモジュールに移動

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,4 +5,11 @@ module Tweet
     client = Client.new
     client.update(text)
   end
+
+  def render_ex_dividend_previous_date(count = nil, symbols_text = nil)
+    <<~TWEET
+      今日までの購入で配当金が受け取れる米国株は「#{count}件」です (配当落ち前日)
+      #{symbols_text}
+    TWEET
+  end
 end
