424bfe16b5456cf3fabf3c12d70e9a7cd91018c1
yoshikouki <yoshikouki@gmail.com>
2021-07-11 10:49:33 +0900

    文字数は適当な修正前提のテストを追加

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -26,5 +26,25 @@ RSpec.describe Tweet, type: :model do
         expect(actual).to eq(expected)
       end
     end
+
+    context "引数に:symbolのキーを持つオブジェクトを渡されてツイート本文が規定文字数を超える場合" do
+      it "規定文字数を超えないようにツイート本文を作成して返す" do
+        dividends = (1..100).map { |i| { symbol: "test#{i}" } }
+        symbols_text = ""
+        dividends.each_with_index do |d, i|
+          symbols_text += "$#{d[:symbol]} "
+          if i + 1 >= 30
+            symbols_text += "...他70件"
+            break
+          end
+        end
+        expected = <<~TWEET
+          今日までの購入で配当金が受け取れる米国株は「#{dividends.count}件」です (配当落ち前日)
+          #{symbols_text}
+        TWEET
+        actual = Tweet.render_ex_dividend_previous_date(dividends)
+        expect(actual).to eq(expected)
+      end
+    end
   end
 end
