c44cecf610a198a2ac734b6686aa04eab03bd96c
yoshikouki <yoshikouki@gmail.com>
2021-07-13 23:34:24 +0900

    インスタンスの属性名をRailsっぽく変更

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 class Dividend
-  attr_accessor :symbol,            # ティッカーシンボル
-                :ex_dividend_date,  # 権利落ち日
-                :record_date,       # 権利確定日
-                :payment_date,      # 支払日
-                :declaration_date,  # 発表日
-                :dividend,          # 配当金
-                :adjusted_dividend  # 調整後配当金
+  attr_accessor :symbol, # ティッカーシンボル
+                :ex_dividend_on, # 権利落ち日
+                :records_on, # 権利確定日
+                :pays_on, # 支払日
+                :declares_on, # 発表日
+                :dividend, # 配当金
+                :adjusted_dividend # 調整後配当金
 
   def initialize(arg)
     @symbol = arg[:symbol]
-    @ex_dividend_date = arg[:ex_dividend_date]
-    @record_date = arg[:record_date]
-    @payment_date = arg[:payment_date]
-    @declaration_date = arg[:declaration_date]
+    @ex_dividend_on = arg[:ex_dividend_on]
+    @records_on = arg[:records_on]
+    @pays_on = arg[:pays_on]
+    @declares_on = arg[:declares_on]
     @dividend = arg[:dividend]
     @adjusted_dividend = arg[:adjusted_dividend]
   end
@@ -32,7 +32,7 @@ class Dividend
     dividends = Client::Fmp.get_dividend_calendar(from: Time.at(3.months.ago))
 
     # 選択
-    dividends = filter_by_condition(dividends, :declaration_date, time)
+    dividends = filter_by_condition(dividends, :declares_on, time)
 
     # View 用に変換
     dividends.map { |dividend| new(dividend) }
@@ -41,7 +41,7 @@ class Dividend
   def self.filter_by_condition(dividends, condition, time)
     dividends.delete_if do |dividend|
       if dividend[condition].present?
-        Time.parse(dividend[:declaration_date]) < time
+        Time.parse(dividend[:declares_on]) < time
       else
         true
       end
