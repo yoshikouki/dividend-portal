0bba321075e1e8b2196592cdb38eb2b03d186b3a
yoshikouki <yoshikouki@gmail.com>
2021-07-13 23:49:24 +0900

    Dividend.recent の返り値はDividendのインスタンスを期待するよね

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -21,10 +21,25 @@ class Dividend
 
   def self.recent(from: nil, to: nil)
     from ||= Time.at(2.days.ago)
-    dividends = Client::Fmp.get_dividend_calendar(
+    row_dividends = Client::Fmp.get_dividend_calendar(
       from: from,
       to: to,
     )
+    to_instances(row_dividends)
+  end
+
+  def self.to_instances(row_dividends = [])
+    row_dividends.map do |dividend|
+      Dividend.new(
+        ex_dividend_on: dividend[:date],
+        records_on: dividend[:record_date],
+        pays_on: dividend[:payment_date],
+        declares_on: dividend[:declaration_date],
+        symbol: dividend[:symbol],
+        dividend: dividend[:dividend],
+        adjusted_dividend: dividend[:adj_dividend],
+      )
+    end
   end
 
   def self.declared_from(time = Time.at(1.week.ago))
--- spec/models/dividend_spec.rb
+++ spec/models/dividend_spec.rb
@@ -55,4 +55,33 @@ RSpec.describe Dividend, type: :model do
       end
     end
   end
+
+  describe ".to_instances" do
+    it "FMPから取得した生データをインスタンスメソッドに変換して返す" do
+      expected = [
+        Dividend.new(
+          ex_dividend_on: "2020-08-07",
+          records_on: "2020-08-10",
+          pays_on: "2020-09-10",
+          declares_on: "2020-07-28",
+          symbol: "IBM",
+          dividend: 1.63,
+          adjusted_dividend: 1.63,
+        ),
+      ]
+      dividends = [
+        { date: "2020-08-07",
+          label: "August 07, 20",
+          adj_dividend: 1.63,
+          symbol: "IBM",
+          dividend: 1.63,
+          record_date: "2020-08-10",
+          payment_date: "2020-09-10",
+          declaration_date: "2020-07-28" },
+      ]
+      actual = Dividend.to_instances(dividends)
+      expect(actual.first.ex_dividend_on).to eq expected.first.ex_dividend_on
+      expect(actual.first.dividend).to eq expected.first.dividend
+    end
+  end
 end
