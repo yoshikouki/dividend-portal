ac35fe076b7b9c23614fa4a1882646f3505accd2
yoshikouki <yoshikouki@gmail.com>
2021-07-13 23:02:40 +0900

    recent_dividends テーブルを作った

---
+++ db/migrate/20210713134430_create_recent_dividends.rb
@@ -0,0 +1,17 @@
+class CreateRecentDividends < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recent_dividends do |t|
+      t.date :ex_dividend_on
+      t.date :records_on
+      t.date :pays_on
+      t.date :declares_on
+      t.string :symbol
+      t.decimal :dividend, precision: 30, scale: 25
+      t.decimal :adjusted_dividend, precision: 30, scale: 25
+
+      t.timestamps
+    end
+
+    add_index :recent_dividends, :symbol
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_619_092_045) do
+ActiveRecord::Schema.define(version: 2021_07_13_134430) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,4 +22,18 @@ ActiveRecord::Schema.define(version: 20_210_619_092_045) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "recent_dividends", force: :cascade do |t|
+    t.date "ex_dividend_on"
+    t.date "records_on"
+    t.date "pays_on"
+    t.date "declares_on"
+    t.string "symbol"
+    t.decimal "dividend", precision: 30, scale: 25
+    t.decimal "adjusted_dividend", precision: 30, scale: 25
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["symbol"], name: "index_recent_dividends_on_symbol"
+  end
+
 end
