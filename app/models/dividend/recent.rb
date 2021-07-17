6da173394607c8484ff4d7ccde01f0f70066cb18
yoshikouki <yoshikouki@gmail.com>
2021-07-17 11:54:14 +0900

    最新情報分のSQLを発行するけどシンプルなやつ

--- app/models/dividend/recent.rb
+++ app/models/dividend/recent.rb
@@ -2,4 +2,16 @@
 
 class Dividend::Recent < ApplicationRecord
   self.table_name =  "dividends"
+
+  def self.update_to_latest
+    latest_dividends = Dividend::Api.recent
+
+    latest_dividends.each do |dividend|
+      Dividend.find_or_create_by(
+        symbol: dividend.symbol,
+        dividend: dividend.dividend,
+        ex_dividend_on: dividend.ex_dividend_on,
+        )
+    end
+  end
 end
