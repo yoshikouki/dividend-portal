506cf1ca4fb95b95a891a02dab07061f4159ee61
yoshikouki <yoshikouki@gmail.com>
2021-07-17 11:53:29 +0900

    Revert "なるべくSQLを発行しない方針の途中までの実装"
    This reverts commit 32fff1d80a2bdba090f4bd8481aae5a1252f32bb.

--- app/models/dividend/recent.rb
+++ app/models/dividend/recent.rb
@@ -2,27 +2,4 @@
 
 class Dividend::Recent < ApplicationRecord
   self.table_name =  "dividends"
-
-  def self.update_to_latest
-    latest_dividends = Dividend::Api.recent
-
-    new_dividends = filter_new_dividends(latest_dividends)
-    new_dividends
-  end
-
-  def self.filter_new_dividends(latest_dividends, current_dividends = all)
-    latest_dividends.map do |latest_dividend|
-      new = nil
-      current_dividends.each do |current_dividend|
-        new = latest_dividend unless current_dividend.same?(latest_dividend)
-      end
-      new
-    end.compact
-  end
-
-  def same?(target_dividend)
-    dividend == target_dividend.dividend &&
-      symbol == target_dividend.symbol &&
-      ex_dividend_on == target_dividend.ex_dividend_on
-  end
 end
