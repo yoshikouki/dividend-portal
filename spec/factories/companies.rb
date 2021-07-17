f7a1969a454ec692bc3ed421f1547e19652bc5bf
yoshikouki <yoshikouki@gmail.com>
2021-07-17 12:39:29 +0900

    企業のテストデータを用意

---
+++ spec/factories/companies.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :company do
+    symbol { "WFC-PZ" }
+    name { "Wells Fargo & Company" }
+    exchange { "New York Stock Exchange" }
+  end
+end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -5,19 +5,15 @@ require "rails_helper"
 RSpec.describe Company, type: :model do
   describe "diff?" do
     context "同じ内容だった場合" do
+      let!(:company) { FactoryBot.create(:company) }
+
       it "false を返す" do
-        arg = {
-          symbol: "TEST",
-          name: "name",
-          exchange: "exchange",
-        }
         check_list = %i[
           name
           exchange
         ]
-        company = Company.new(arg)
-        same_company = Company.new(arg)
-        expect(company.diff?(same_company, check_list)).to be_falsey
+        same_company = Company.new(symbol: "WFC-PZ", name: "Wells Fargo & Company", exchange: "New York Stock Exchange")
+        expect(company.diff?(same_company, check_list)).to be false
       end
     end
   end
