4a641a2740cb1aa09b55c01bda6b246b7ee5d4a9
yoshikouki <yoshikouki@gmail.com>
2021-07-20 22:18:34 +0900

    notified カラムを追加

---
+++ db/migrate/20210720131650_add_notified_to_dividend.rb
@@ -0,0 +1,5 @@
+class AddNotifiedToDividend < ActiveRecord::Migration[6.1]
+  def change
+    add_column :dividends, :notified, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_717_102_126) do
+ActiveRecord::Schema.define(version: 2021_07_20_131650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20_210_717_102_126) do
     t.decimal "adjusted_dividend", precision: 30, scale: 25
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "notified", default: false
     t.index ["symbol"], name: "index_dividends_on_symbol"
   end
 end
