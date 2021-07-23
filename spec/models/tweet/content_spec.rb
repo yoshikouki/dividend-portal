b007c825bb353339d316fc8a1659024a56ff77ae
yoshikouki <yoshikouki@gmail.com>
2021-07-23 17:00:52 +0900

    Fix Test

--- spec/models/tweet/content_spec.rb
+++ spec/models/tweet/content_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe Tweet::Content, type: :model do
     context "引数に:symbolのキーを持つオブジェクトを渡された場合" do
       it "オブジェクトの個数と:symbolの値からツイート本文を作成して返す" do
         dividends = [
-          { symbol: "test" },
-          { symbol: "dividend" },
-          { symbol: "portal" },
+          Dividend.new(symbol: "test"),
+          Dividend.new(symbol: "dividend"),
+          Dividend.new(symbol: "portal"),
         ]
         symbols_text = dividends.map { |d| "$#{d[:symbol]}" }.join(" ")
         expected = "今日までの購入で配当金が受け取れる米国株は「#{dividends.count}件」です (配当落ち前日)\n#{symbols_text}\n"
@@ -31,10 +31,10 @@ RSpec.describe Tweet::Content, type: :model do
 
     context "引数に:symbolのキーを持つオブジェクトを渡されてツイート本文が規定文字数を超える場合" do
       it "規定文字数を超えないようにツイート本文を作成して返す" do
-        dividends = (1..100).map { |i| { symbol: "test#{i}" } }
+        dividends = (1..100).map { |i| Dividend.new(symbol: "test#{i}") }
         max_count = 22
         symbols_text = dividends[0..(max_count - 1)].map { |d| "$#{d[:symbol]}" }.join(" ")
-        symbols_text += "...他#{dividends.count - max_count}件"
+        symbols_text += " ...他#{dividends.count - max_count}件"
         expected = <<~TWEET
           今日までの購入で配当金が受け取れる米国株は「#{dividends.count}件」です (配当落ち前日)
           #{symbols_text}
