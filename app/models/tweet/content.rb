3f043970f45cfb8adf63e2c033311dd799d448c2
yoshikouki <yoshikouki@gmail.com>
2021-07-23 17:01:01 +0900

    リファクタリング

--- app/models/tweet/content.rb
+++ app/models/tweet/content.rb
@@ -13,25 +13,10 @@ module Tweet
     end
 
     def ex_dividend_previous_date
-      content_for_calculation = template_for_ex_dividend_previous_date(dividends.count)
-      tweet_symbols = []
+      front_part = "今日までの購入で配当金が受け取れる米国株は「#{dividends.count}件」です (配当落ち前日)"
+      return front_part if dividends.count.zero?
 
-      dividends.each do |dividend|
-        content_for_calculation += "$#{dividend[:symbol]} "
-        break if Twitter::TwitterText::Validation.parse_tweet(content_for_calculation)[:weighted_length] > 240
-
-        tweet_symbols << dividend[:symbol]
-      end
-      remaining_count = dividends.count - tweet_symbols.count
-      template_for_ex_dividend_previous_date(dividends.count, tweet_symbols, remaining_count)
-    end
-
-    def template_for_ex_dividend_previous_date(dividends_count = 0, tweet_symbols = [], remaining_count = 0)
-      front_part = "今日までの購入で配当金が受け取れる米国株は「#{dividends_count}件」です (配当落ち前日)"
-      return front_part if dividends_count.zero?
-
-      symbols_part = tweet_symbols.map { |s| "$#{s}" }.join(" ")
-      symbols_part += "...他#{remaining_count}件" if remaining_count.positive?
+      symbols_part = render_symbols_part(front_part, 240)
 
       <<~TWEET
         #{front_part}
