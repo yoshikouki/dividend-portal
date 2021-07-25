4736577ad3c972d12196584db0f13a0206d8b25d
yoshikouki <yoshikouki@gmail.com>
2021-07-25 17:36:23 +0900

    コンテンツ文面をTemplateで管理する

--- app/models/tweet/content/dividend.rb
+++ app/models/tweet/content/dividend.rb
@@ -16,13 +16,13 @@ module Tweet
       end
 
       def ex_dividend_previous_date
-        @content.main_section = "今日までの購入で配当金が受け取れる米国株は「#{dividends.count}件」です (配当落ち前日)"
+        @content.main_section = Template.dividend_ex_dividend_previous_date(dividends.count)
         @content.footer_section = render_symbols_part if dividends.count.positive?
         @content.render
       end
 
       def latest_dividend
-        @content.main_section = "米国株に関する新着の配当金情報は #{dividends.count}件です"
+        @content.main_section = Template.dividend_latest_dividend(dividends.count)
         @content.footer_section = render_symbols_part if dividends.count.positive?
         @content.render
       end
@@ -35,13 +35,13 @@ module Tweet
         length ||= calculate_symbols_part_length
         symbols = shift_symbols_in_number_of_characters(length)
 
-        symbols_part = symbols.map { |symbol| "$#{symbol}" }.join(" ")
-        symbols_part += " ...残り#{remained_dividends.count}件" if remained?
+        symbols_part = Template.symbols_section(symbols)
+        symbols_part += Template.remained_section(remained_dividends.count) if remained?
         symbols_part
       end
 
       def calculate_symbols_part_length
-        max_remained_part_length = Content.weighted_length(" ...残り#{dividends.count}件")
+        max_remained_part_length = Content.weighted_length(Template.remained_section(dividends.count))
         Content::MAX_WEIGHTED_LENGTH - max_remained_part_length - @content.weighted_length
       end
 
@@ -49,7 +49,7 @@ module Tweet
         symbols_text_for_calculation = ""
         shift_number = 0
         remained_dividends.each.with_index(1) do |dividend, index|
-          symbols_text_for_calculation += "$#{dividend.symbol} "
+          symbols_text_for_calculation += Template.symbol(dividend.symbol) + Template::SYMBOL_DELIMITER
           break if Content.weighted_length(symbols_text_for_calculation) > limited
 
           shift_number = index
