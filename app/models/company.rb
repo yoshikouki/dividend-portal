6e76e2fa3c38f8aee154ddff85a2a70e93e65ad8
yoshikouki <yoshikouki@gmail.com>
2021-08-01 09:38:21 +0900

    テスタブルにするためモデルに移管

--- app/models/company.rb
+++ app/models/company.rb
@@ -14,4 +14,44 @@ class Company < ApplicationRecord
     end
     false
   end
+
+  def self.update_to_least
+    current_all = Company.all.to_a
+    latest_all = Client::Fmp.get_symbols_list
+    new_coming = []
+    needs_updating = []
+
+    latest_all.each do |raw|
+      latest = {
+        symbol: raw["symbol"],
+        name: raw["name"],
+        exchange: raw["exchange"],
+      }
+      target = current_all.find_index { |cc| cc.symbol == latest[:symbol] }
+
+      # 未知の企業ならインサートする
+      if target.nil?
+        new_coming << {
+          **latest,
+          created_at: Time.current,
+          updated_at: Time.current,
+        }
+        next
+      end
+
+      current = current_all.slice!(target)
+      # 情報を比較して差異があれば更新
+      next unless current.diff?(latest, %i[name exchange])
+
+      needs_updating << {
+        **current.attributes.symbolize_keys,
+        **latest,
+      }
+    end
+
+    Company.insert_all!(new_coming) if new_coming.count.positive?
+    Company.upsert_all(needs_updating) if needs_updating.count.positive?
+
+    puts "inserted: #{new_coming.count} \nupdated: #{needs_updating.count}"
+  end
 end
