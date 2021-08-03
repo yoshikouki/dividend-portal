b81ea257c384221cad9c19fe88b30f350b56a176
yoshikouki <yoshikouki@gmail.com>
2021-08-03 22:38:31 +0900

    FMP から取得したデータをCompany用のHashに変換する

--- app/models/company/api.rb
+++ app/models/company/api.rb
@@ -2,11 +2,37 @@
 
 class Company
   module Api
+    PROFILE_CONVERSION = {
+      symbol: :symbol,
+      company_name: :name,
+      currency: :currency,
+      exchange: :exchange,
+      exchange_short_name: :exchange_short_name,
+      industry: :industry,
+      sector: :sector,
+      country: :country,
+      image: :image,
+      ipo_date: :ipo_date,
+    }.freeze
+
     def self.fetch_us
       response = Client::Fmp.get_symbols_list
       response.map do |r|
         r.delete(:price)
       end
+    end
+
+    def self.profiles(symbol)
+      return [] unless symbol
+
+      row_profiles = Client::Fmp::profile(symbol)
+      row_profiles.map do |raw_profile|
+        convert_profile_response(raw_profile)
+      end
+    end
+
+    def self.convert_profile_response(raw_profile)
+      PROFILE_CONVERSION.map { |k, v| [v, raw_profile[k]] }.to_h
     end
   end
 end
--- spec/models/company/api_test.rb
+++ spec/models/company/api_test.rb
@@ -25,4 +25,63 @@ RSpec.describe Company::Api, type: :model do
       expect(actual.count).to eq 3
     end
   end
+
+  describe ".profile" do
+    let!(:api_response) do
+      [{ symbol: "KO",
+         price: 56.88,
+         beta: 0.614254,
+         vol_avg: 14_358_100,
+         mkt_cap: 245_529_329_664,
+         last_div: 1.66,
+         range: "46.22-57.56",
+         changes: -0.15,
+         company_name: "The Coca-Cola Company",
+         currency: "USD",
+         cik: "0000021344",
+         isin: "US1912161007",
+         cusip: "191216100",
+         exchange: "New York Stock Exchange",
+         exchange_short_name: "NYSE",
+         industry: "Beverages—Non-Alcoholic",
+         website: "http://www.coca-colacompany.com",
+         description: "The Coca-Cola Company, a beverage company, manufactures, markets, and sells various nonalcoholic beverages worldwide.",
+         ceo: "Mr. James Quincey",
+         sector: "Consumer Defensive",
+         country: "US",
+         full_time_employees: "80300",
+         phone: "14046762121",
+         address: "1 Coca Cola Plz NW",
+         city: "Atlanta",
+         state: "GEORGIA",
+         zip: "30301",
+         dcf_diff: -22.44,
+         dcf: 58.4493,
+         image: "https://financialmodelingprep.com/image-stock/KO.png",
+         ipo_date: "1919-09-05",
+         default_image: false,
+         is_etf: false,
+         is_actively_trading: true }]
+    end
+
+    it "FMPから取得した生データをCompanyパラメタのHashに変換して返す" do
+      allow(Client::Fmp).to receive(:profile).and_return(api_response)
+      expected = [
+        symbol: "KO",
+        company_name: "The Coca-Cola Company",
+        currency: "USD",
+        exchange: "New York Stock Exchange",
+        exchange_short_name: "NYSE",
+        industry: "Beverages—Non-Alcoholic",
+        website: "http://www.coca-colacompany.com",
+        description: "The Coca-Cola Company, a beverage company, manufactures, markets, and sells various nonalcoholic beverages worldwide.",
+        sector: "Consumer Defensive",
+        country: "US",
+        image: "https://financialmodelingprep.com/image-stock/KO.png",
+        ipo_date: "1919-09-05",
+      ]
+      actual = Company::Api.profile("KO")
+      expect(actual).to eq expected
+    end
+  end
 end
