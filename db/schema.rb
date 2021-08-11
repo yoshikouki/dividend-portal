42f40b502f9f44605296313f74e8e2fa5642d301
yoshikouki <yoshikouki@gmail.com>
2021-08-12 00:43:48 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_090606) do
+ActiveRecord::Schema.define(version: 2021_08_11_154222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 2021_08_11_090606) do
     t.bigint "company_id", null: false
     t.index ["company_id"], name: "index_dividends_on_company_id"
     t.index ["symbol"], name: "index_dividends_on_symbol"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "dividends", "companies"
