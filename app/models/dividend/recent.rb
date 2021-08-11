31882c7a052f10383de8f49e4c4be426c62bc6d2
yoshikouki <yoshikouki@gmail.com>
2021-08-11 22:45:55 +0900

    カレンダーが複数なのは英語的に変

--- app/models/dividend/recent.rb
+++ app/models/dividend/recent.rb
@@ -9,9 +9,9 @@ class Dividend
       update_us_to_latest
     end
 
-    def self.update_to_latest(latest_dividends = Dividend::Api.recent)
+    def self.update_to_latest(latest_dividend_calendar = Dividend::Api.recent)
       current_dividends = Dividend.order(:ex_dividend_on).to_a
-      new_dividends = latest_dividends.filter_map do |latest|
+      new_dividends = latest_dividend_calendar.filter_map do |latest|
         latest = remove_empty_string(latest)
         current_index = current_dividends.find_index { |current| current.same?(latest) }
         latest.merge(created_at: Time.current, updated_at: Time.current) unless current_index
@@ -21,9 +21,9 @@ class Dividend
     end
 
     def self.update_us_to_latest
-      dividend_calendars = Api.recent
-      dividend_calendars_in_us = associate_with_us_companies(dividend_calendars)
-      update_to_latest(dividend_calendars_in_us)
+      latest_dividend_calendar = Api.recent
+      dividend_calendar_in_us = associate_with_us_companies(latest_dividend_calendar)
+      update_to_latest(dividend_calendar_in_us)
     end
 
     def self.destroy_outdated
@@ -31,12 +31,12 @@ class Dividend
       Dividend.where(ex_dividend_on: ..outdated).destroy_all
     end
 
-    def self.associate_with_us_companies(dividend_calendars = [])
-      symbols = dividend_calendars.pluck(:symbol)
+    def self.associate_with_us_companies(dividend_calendar = [])
+      symbols = dividend_calendar.pluck(:symbol)
       companies_in_us = Company.in_us_where_or_create_by_symbol(symbols).to_a
       symbols_in_us = companies_in_us.pluck(:symbol)
 
-      dividend_calendars.filter_map do |dc|
+      dividend_calendar.filter_map do |dc|
         symbol = dc[:symbol]
         index = symbols_in_us.find_index(symbol)
         if index
