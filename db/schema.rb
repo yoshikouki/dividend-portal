094509cd86eb0b90b064712b952e33b507e2f04a
yoshikouki <yoshikouki@gmail.com>
2021-08-12 00:44:07 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_154222) do
+ActiveRecord::Schema.define(version: 2021_08_11_154350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2021_08_11_154222) do
     t.string "image"
     t.integer "years_of_dividend_growth"
     t.index ["symbol"], name: "index_companies_on_symbol"
+  end
+
+  create_table "company_tags", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_company_tags_on_company_id"
+    t.index ["tag_id"], name: "index_company_tags_on_tag_id"
   end
 
   create_table "dividends", force: :cascade do |t|
@@ -54,5 +63,7 @@ ActiveRecord::Schema.define(version: 2021_08_11_154222) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "company_tags", "companies"
+  add_foreign_key "company_tags", "tags"
   add_foreign_key "dividends", "companies"
 end
