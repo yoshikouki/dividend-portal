5f7a22a3cb90b023412649222e7b177a8ea0e004
yoshikouki <yoshikouki@gmail.com>
2021-08-12 17:02:39 +0900

    Tag.name の末尾に数字は使えるし、一文字でもOKにした

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,12 +4,12 @@ class Tag < ApplicationRecord
   has_many :company_tags
   has_many :companies, through: :company_tags
 
-  REGEXP_NAME = /\A[a-zA-Z]\w*[a-zA-Z]\z/i
+  REGEXP_NAME = /\A([a-zA-Z]\w*[a-zA-Z0-9]|[a-zA-Z])\z/i
 
   validates :name,
             presence: true,
             uniqueness: true,
-            length: { minimum: 2 },
+            length: { minimum: 1 },
             format: { with: REGEXP_NAME }
 
   validates :display_name,
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Tag, type: :model do
       it "valid" do
         [
           { name: :dividend_aristocrats, display_name: "配当貴族" },
-          { name: :aa, display_name: "テスト" },
+          { name: :a, display_name: "1文字の識別子" },
+          { name: :a1, display_name: "数字は先頭以外" },
         ].each do |attr|
           expect(Tag.new(attr).valid?).to be true
         end
@@ -18,11 +19,11 @@ RSpec.describe Tag, type: :model do
     context "異常系" do
       it "invalid" do
         [
-          { name: :aa, display_name: "" },
-          { name: :a, display_name: "2文字以下の識別子" },
+          { name: :empty_display_name, display_name: "" },
           { name: :_a, display_name: "アンダーバーから始まる" },
           { name: :a_, display_name: "アンダーバーで終わる" },
           { name: "a-a", display_name: "アンダーバー以外の記号" },
+          { name: "1a", display_name: "先頭に数字" },
         ].each do |attr|
           expect(Tag.new(attr).valid?).to be false
         end
