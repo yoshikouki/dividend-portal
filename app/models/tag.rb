84f58d0b61b45350d49d8a2a46a2c390cd386093
yoshikouki <yoshikouki@gmail.com>
2021-08-12 15:42:12 +0900

    バリデーションを追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,16 @@
 class Tag < ApplicationRecord
   has_many :company_tags
   has_many :companies, through: :company_tags
+
+  REGEXP_NAME = /\A[a-zA-Z]\w*[a-zA-Z]\z/i
+
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 2 },
+            format: { with: REGEXP_NAME }
+
+  validates :display_name,
+            presence: true,
+            uniqueness: true
 end
