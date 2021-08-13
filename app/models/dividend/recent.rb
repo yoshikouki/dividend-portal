6edfd7cf11391ce0359d78038ff7db7017bc7e5b
yoshikouki <yoshikouki@gmail.com>
2021-08-13 12:16:49 +0900

    返り値が分かりづらいのでYARDコメント追加

--- app/models/dividend/recent.rb
+++ app/models/dividend/recent.rb
@@ -9,6 +9,7 @@ class Dividend
       update_us_to_latest
     end
 
+    # @return [ActiveRecord::Result, nil]
     def self.update_to_latest(latest_dividend_calendar)
       current_dividends = Dividend.order(:ex_dividend_on).to_a
       new_dividends = latest_dividend_calendar.filter_map do |latest|
