bf8dbba03184c86244292335fd729e11c61fd250
yoshikouki <yoshikouki@gmail.com>
2021-08-13 11:17:05 +0900

    g model ReportQueueOfDividendAristocratsDividend

---
+++ app/models/report_queue_of_dividend_aristocrats_dividend.rb
@@ -0,0 +1,2 @@
+class ReportQueueOfDividendAristocratsDividend < ReportQueue
+end
---
+++ spec/factories/report_queue_of_dividend_aristocrats_dividends.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :report_queue_of_dividend_aristocrats_dividend do
+    
+  end
+end
