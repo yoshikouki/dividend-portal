e83ac6f1725322b2eb2c6e6cbbe8a9672d8b689a
yoshikouki <yoshikouki@gmail.com>
2021-08-14 23:40:47 +0900

    作るテストデータを間違っていた

--- spec/models/report_queue_of_dividend_aristocrats_dividend_spec.rb
+++ spec/models/report_queue_of_dividend_aristocrats_dividend_spec.rb
@@ -35,9 +35,9 @@ RSpec.describe ReportQueueOfDividendAristocratsDividend, type: :model do
 
   describe ".dequeue" do
     context "正常系" do
-      let!(:no_dividend_aristocrats) { FactoryBot.create(:dividend, :with_company) }
-      let!(:dividend) { FactoryBot.create(:dividend, :with_dividend_aristocrats_company) }
-      let!(:dividend2) { FactoryBot.create(:dividend, :with_dividend_aristocrats_company) }
+      let!(:no_dividend_aristocrats) { FactoryBot.create(:report_queue, :with_no_dividend_aristocrats_dividend) }
+      let!(:dividend_aristocrats_queue) { FactoryBot.create(:report_queue_of_dividend_aristocrats_dividend, :with_dividend_aristocrats_dividend) }
+      let!(:dividend_aristocrats_queue2) { FactoryBot.create(:report_queue_of_dividend_aristocrats_dividend, :with_dividend_aristocrats_dividend) }
 
       it "最新の配当貴族の配当レポートキューが一つ削除される" do
         expect { ReportQueueOfDividendAristocratsDividend.dequeue }.to change(ReportQueue, :count).by(-1)
@@ -46,7 +46,7 @@ RSpec.describe ReportQueueOfDividendAristocratsDividend, type: :model do
     end
 
     context "配当貴族ではない配当レポートキューだけしかない場合" do
-      let!(:no_dividend_aristocrats) { FactoryBot.create(:dividend, :with_company) }
+      let!(:no_dividend_aristocrats) { FactoryBot.create(:report_queue, :with_no_dividend_aristocrats_dividend) }
 
       it "キューは削除されない" do
         expect { ReportQueueOfDividendAristocratsDividend.dequeue }.to change(ReportQueue, :count).by(0)
