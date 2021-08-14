9df2c3ab36229e0ca095d26db39a7a7dc4c957d0
yoshikouki <yoshikouki@gmail.com>
2021-08-14 22:50:58 +0900

    配当貴族の場合はキューに追加する

--- app/models/report_queue_of_dividend_aristocrats_dividend.rb
+++ app/models/report_queue_of_dividend_aristocrats_dividend.rb
@@ -3,10 +3,13 @@
 class ReportQueueOfDividendAristocratsDividend < ReportQueue
   class << self
     def enqueue(dividend_ids: nil)
-      queues = dividend_ids.map do |dividend_id|
+      dividend_aristocrats_dividends = Dividend.dividend_aristocrats.where(id: dividend_ids)
+      queues = dividend_aristocrats_dividends.pluck(:id).map do |dividend_id|
         now = Time.current
         { dividend_id: dividend_id, type: self, created_at: now, updated_at: now }
       end
+      return if queues.empty?
+
       insert_all(queues)
     end
   end
