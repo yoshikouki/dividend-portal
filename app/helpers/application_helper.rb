6841d86b9ec97d840ff95746ca0c418f57638826
yoshikouki <yoshikouki@gmail.com>
2021-08-21 22:59:48 +0900

    Float をパーセント表示する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def float_to_percentage(float, precision: 2)
+    percentage = (BigDecimal(float, 10) * 100).to_f
+    number_to_percentage(percentage, precision: 2)
+  end
 end
