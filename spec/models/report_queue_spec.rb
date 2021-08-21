893ba0e0a25b6110f4589f96621a3fa6060393d8
yoshikouki <yoshikouki@gmail.com>
2021-08-21 23:51:37 +0900

    テスト追加していなかった

--- spec/models/report_queue_spec.rb
+++ spec/models/report_queue_spec.rb
@@ -3,15 +3,15 @@
 require "rails_helper"
 
 RSpec.describe ReportQueue, type: :model do
-  describe ".enqueue" do
-    context "正常系" do
-      it "キューを追加する"
-    end
-  end
-
   describe ".dequeue" do
     context "正常系" do
-      it "キューを取り出してレコードを削除する"
+      let!(:report_queue) { FactoryBot.create(:report_queue, dividend: FactoryBot.create(:dividend, :with_company)) }
+
+      it "キューとして最初のレコードを削除する" do
+        deleted_report_queue = ReportQueue.dequeue
+        expect(ReportQueue.count).to eq(0)
+        expect(deleted_report_queue).to eq(report_queue)
+      end
     end
   end
 end
