1cea805ac21c7fb57d9f4ed3371e6722e00679f5
yoshikouki <yoshikouki@gmail.com>
2021-08-21 12:13:14 +0900

    配当金の概要情報を取得する

--- app/models/dividend/api.rb
+++ app/models/dividend/api.rb
@@ -14,6 +14,15 @@ class Dividend
       adj_dividend: :adjusted_dividend,
     }.freeze
 
+    CONVERSION_TABLE_OF_OUTLOOK = {
+      symbol: :symbol,
+      price: :price,
+      dividend_yiel_ttm: :dividend_yield,
+      dividend_per_share_ttm: :dividend_per_share,
+      stock_dividend: :dividends, # stock_dividend (株式配当 ※通貨による配当ではない) ではなかったので改名
+      payout_ratio_ttm: :payout_ratio
+    }.freeze
+
     def self.recent(from: RECENT_REFERENCE_START_DATE, to: nil)
       row_dividends = Client::Fmp.get_dividend_calendar(
         from: from,
@@ -46,6 +55,24 @@ class Dividend
         dividend = CONVERSION_TABLE_OF_DIVIDEND_CALENDAR.filter_map { |k, v| [v, hd[k]] if hd[k] }.to_h
         dividend.merge(symbol: historical_dividends[:symbol])
       end
+    end
+
+    def self.outlook(symbol)
+      response = Client::Fmp.company_outlook(symbol)
+      convert_response_of_company_outlook(response)
+    end
+
+    def self.convert_response_of_company_outlook(response)
+      {
+        symbol: response[:profile][:symbol],
+        price: response[:profile][:price],
+        ttm: {
+          dividend_yield: response[:ratios].first[:dividend_yiel_ttm],
+          dividend_per_share: response[:ratios].first[:dividend_per_share_ttm],
+          payout_ratio: response[:ratios].first[:dividend_yiel_ttm],
+        },
+        dividends: response[:stock_dividend],
+      }
     end
   end
 end
