03e4d4c31e7c9b20b27ede6b419d5b19f33e318b
yoshikouki <yoshikouki@gmail.com>
2021-08-29 11:13:16 +0900

    VCR でテスト

--- spec/models/dividend/api_spec.rb
+++ spec/models/dividend/api_spec.rb
@@ -3,6 +3,48 @@
 require "rails_helper"
 
 RSpec.describe Dividend::Api, type: :model do
+  describe ".recent" do
+    let!(:expected_keys) do
+      [:ex_dividend_on, :records_on, :pays_on, :declares_on, :symbol, :dividend, :adjusted_dividend]
+    end
+
+    it "期間指定してAPI経由で配当情報を取得できる" do
+      VCR.use_cassette("models/dividend/api/recent") do
+        actual = Dividend::Api.recent
+        expect(actual.class).to eq Array
+        expect(actual[0].keys).to eq expected_keys
+      end
+    end
+  end
+
+  describe ".all" do
+    let!(:expected_keys) do
+      [:ex_dividend_on, :records_on, :pays_on, :declares_on, :dividend, :adjusted_dividend, :symbol]
+    end
+
+    context "シンボルが単数の場合" do
+      it "期間指定してAPI経由で配当情報を取得できる" do
+        VCR.use_cassette("models/dividend/api/all") do
+          actual = Dividend::Api.all("KO")
+          expect(actual.class).to eq Array
+          expect(actual[0].keys).to eq expected_keys
+        end
+      end
+    end
+  end
+
+  describe ".outlook" do
+    context "シンボルが単数の場合" do
+      it "期間指定してAPI経由で配当情報を取得できる" do
+        VCR.use_cassette("models/dividend/api/outlook") do
+          actual = Dividend::Api.outlook("KO")
+          expect(actual.class).to eq Hash
+          expect(actual.keys).to eq [:symbol, :price, :ttm, :dividends]
+        end
+      end
+    end
+  end
+
   describe ".convert_response_of_dividend_calendar" do
     it "FMPのレスポンスをハッシュに変換するして返す" do
       expected = [
