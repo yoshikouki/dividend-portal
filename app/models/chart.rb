0dfd12c4646472bea8cdd0bfaf7f30f0e7146b0a
yoshikouki <yoshikouki@gmail.com>
2021-08-30 21:44:43 +0900

    QuickChart に関する処理をラップする

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,57 +1,14 @@
 # frozen_string_literal: true
 
 class Chart
-  attr_writer :client
+  attr_accessor :quick_chart_client
 
-  TEMP_IMAGE_PATH = "tmp/mixed_chart.png"
-
-  # https://quickchart.io/documentation/
-  QUICK_CHART_DEFAULT_ARG = {
-    width: 500,
-    height: 300,
-    background_color: "#ffffff",
-    device_pixel_ratio: 2.0,
-    format: "png",
-    key: nil,
-  }.freeze
-
-  def mixed
-    client.to_file(TEMP_IMAGE_PATH)
-    File.new(TEMP_IMAGE_PATH)
+  def initialize(client: QuickChartWrapper.new)
+    @quick_chart_client = client
   end
 
   def new_dividend_of_dividend_aristocrats(dividends)
     dividends_in_chronological_order = dividends.reverse
-    labels = dividends_in_chronological_order.pluck(:ex_dividend_on)
-    dividends_data = dividends_in_chronological_order.pluck(:adjusted_dividend)
-    config = {
-      type: "bar",
-      data: {
-        labels: labels,
-        datasets: [
-          { label: "一株当たり配当($)", data: dividends_data, display: false },
-        ],
-      },
-      options: {
-        title: { text: "$#{dividends[0][:symbol]} 過去25年間の推移", display: true, fontSize: 20, fontFamily: "Sans CJK JP" },
-        scales: {
-          xAxes: [
-            { gridLines: { display: false } },
-          ],
-          yAxes: [
-            { ticks: { beginAtZero: true } },
-          ],
-        },
-        legend: { position: "bottom" },
-      },
-    }
-    client(config).to_file(TEMP_IMAGE_PATH)
-    File.new(TEMP_IMAGE_PATH)
-  end
-
-  private
-
-  def client(config, arg: QUICK_CHART_DEFAULT_ARG)
-    @client ||= QuickChart.new(config, **arg)
+    quick_chart_client.new_dividend_of_dividend_aristocrats(dividends_in_chronological_order)
   end
 end
---
+++ app/models/quick_chart_wrapper.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class QuickChartWrapper
+  TEMP_IMAGE_PATH = "tmp/mixed_chart.png"
+
+  # https://quickchart.io/documentation/
+  QUICK_CHART_DEFAULT_ARG = {
+    width: 500,
+    height: 300,
+    background_color: "#ffffff",
+    device_pixel_ratio: 2.0,
+    format: "png",
+    key: nil,
+  }.freeze
+
+  def new_dividend_of_dividend_aristocrats(dividends)
+    labels = dividends.pluck(:ex_dividend_on)
+    dividends_data = dividends.pluck(:adjusted_dividend)
+    config = {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [
+          { label: "一株当たり配当($)", data: dividends_data, display: false },
+        ],
+      },
+      options: {
+        title: { text: "$#{dividends[0][:symbol]} 過去25年間の推移", display: true, fontSize: 20, fontFamily: "Sans CJK JP" },
+        scales: {
+          xAxes: [
+            { gridLines: { display: false } },
+          ],
+          yAxes: [
+            { ticks: { beginAtZero: true } },
+          ],
+        },
+        legend: { position: "bottom" },
+      },
+    }
+    client(config).to_file(TEMP_IMAGE_PATH)
+    File.new(TEMP_IMAGE_PATH)
+  end
+
+  private
+
+  def client(config, arg: QUICK_CHART_DEFAULT_ARG)
+    @client ||= QuickChart.new(config, **arg)
+  end
+end
