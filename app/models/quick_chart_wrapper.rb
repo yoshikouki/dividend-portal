f245593c084b414fcd9c9f856ab0529b97693bc0
yoshikouki <yoshikouki@gmail.com>
2021-08-30 22:25:27 +0900

    QuickChart レンダリングのリファクタリング

--- app/models/quick_chart_wrapper.rb
+++ app/models/quick_chart_wrapper.rb
@@ -15,35 +15,57 @@ class QuickChartWrapper
 
   def new_dividend_of_dividend_aristocrats(dividends)
     labels = dividends.pluck(:ex_dividend_on)
-    dividends_data = dividends.pluck(:adjusted_dividend)
-    config = {
-      type: "bar",
-      data: {
-        labels: labels,
-        datasets: [
-          { label: "一株当たり配当($)", data: dividends_data, display: false },
-        ],
-      },
-      options: {
-        title: { text: "$#{dividends[0][:symbol]} 過去25年間の推移", display: true, fontSize: 20, fontFamily: "Sans CJK JP" },
-        scales: {
-          xAxes: [
-            { gridLines: { display: false } },
-          ],
-          yAxes: [
-            { ticks: { beginAtZero: true } },
-          ],
-        },
-        legend: { position: "bottom" },
-      },
-    }
-    client(config).to_file(TEMP_IMAGE_PATH)
-    File.new(TEMP_IMAGE_PATH)
+    datasets = [{ label: "一株当たり配当($)", data: dividends.pluck(:adjusted_dividend) }]
+    data = data(labels, datasets)
+    options = options(title: "$#{dividends[0][:symbol]} 過去25年間の推移")
+
+    render config(:bar, data, options)
   end
 
   private
 
+  def data(labels, datasets)
+    { labels: labels,
+      datasets: datasets }
+  end
+
+  def options(title: nil)
+    options = {
+      scales: {
+        xAxes: [
+          { gridLines: { display: false } },
+        ],
+        yAxes: [
+          { ticks: { beginAtZero: true } },
+        ],
+      },
+      legend: { position: "bottom" },
+    }
+    options.merge!(title_hash(title)) if title
+    options
+  end
+
+  def title_hash(title)
+    { title: {
+      text: title,
+      display: true,
+      fontSize: 20,
+      fontFamily: "Sans CJK JP"
+      } }
+  end
+
+  def config(type, data, options)
+    { type: type.to_s,
+      data: data,
+      options: options }
+  end
+
+  def render(config)
+    client(config).to_file(TEMP_IMAGE_PATH)
+    File.new(TEMP_IMAGE_PATH)
+  end
+
   def client(config, arg: QUICK_CHART_DEFAULT_ARG)
-    @client ||= QuickChart.new(config, **arg)
+    QuickChart.new(config, **arg)
   end
 end
