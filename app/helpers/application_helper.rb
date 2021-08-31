2464087f2b1d2b895ff328cf353ec9ed420496f8
yoshikouki <yoshikouki@gmail.com>
2021-08-31 09:17:26 +0900

    増配時に+$の表示に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     percentage = (BigDecimal(float, 10) * 100).to_f
     number_to_percentage(percentage, precision: precision)
   end
+
+  def dollar_display_with_increase_or_decrease(int)
+    sign = if int.positive?
+      "+"
+    elsif int.negative?
+      "-"
+    end
+    "#{sign}$#{int}"
+  end
 end
