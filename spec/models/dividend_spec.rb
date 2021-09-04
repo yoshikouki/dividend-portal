b2f19bef749579bac8a3bec61773015f51b401f8
yoshikouki <yoshikouki@gmail.com>
2021-09-04 23:35:57 +0900

    Fmp::DividendCalendar からの情報で insert_all! 出来る

--- spec/models/dividend_spec.rb
+++ spec/models/dividend_spec.rb
@@ -193,4 +193,63 @@ RSpec.describe Dividend, type: :model do
       end
     end
   end
+
+  describe ".insert_all_from_dividend_calendar!" do
+    context "Company と関連付けた Dividend をバルクインサートする (デフォルト)" do
+      let!(:company) { FactoryBot.create(:company) }
+      let!(:new_company) { FactoryBot.create(:company, symbol: "NEWSYMBOL") }
+      let!(:dividend) do
+        {
+          ex_dividend_on: Date.today.strftime("%Y-%m-%d"),
+          records_on: Date.tomorrow.strftime("%Y-%m-%d"),
+          pays_on: Date.today.next_month.strftime("%Y-%m-%d"),
+          declares_on: Date.today.last_month.strftime("%Y-%m-%d"),
+          symbol: company.symbol,
+          dividend: 0.1,
+          adjusted_dividend: 0.1,
+          company_id: company.id,
+        }
+      end
+
+      it "企業がなかった場合は新しく Company を作成して Dividend をインサートする" do
+        Dividend.create!(dividend)
+        dividend_calendar = [
+          dividend,
+          dividend.merge(symbol: new_company.symbol),
+          dividend.merge(symbol: "KO"),
+        ]
+        VCR.use_cassette("models/dividend/insert_all_from_dividend_calendar/associate_company") do
+          expect { Dividend.insert_all_from_dividend_calendar!(dividend_calendar) }
+            .to change { Dividend.count }.by(3).and change { Company.count }.by(1)
+        end
+      end
+
+      it "権利落ち日・シンボルが同一の配当は無視する"
+    end
+
+    context "Company との関連付けをスキップする" do
+      let!(:dividend) do
+        {
+          ex_dividend_on: Date.today.strftime("%Y-%m-%d"),
+          records_on: nil,
+          pays_on: Date.today.next_month.strftime("%Y-%m-%d"),
+          declares_on: "",
+          symbol: "SYMBOL",
+          dividend: 0.1,
+          adjusted_dividend: nil,
+        }
+      end
+
+      it "空文字は nil として扱って新しいデータを追加する" do
+        Dividend.create!(dividend)
+        dividend_calendar = [
+          dividend,
+          dividend.merge(symbol: "NEWCOMPANY"),
+          dividend.merge(declares_on: "", symbol: "", pays_on: ""), # APIレスポンスがnullの場合に、変換処理で空文字になることがあった
+        ]
+        expect { Dividend.insert_all_from_dividend_calendar!(dividend_calendar, associate_company: false) }
+          .to change { Dividend.count }.by(3).and change { Company.count }.by(0)
+      end
+    end
+  end
 end
