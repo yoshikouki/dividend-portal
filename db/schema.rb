cf532c8f1508e57c011bdd031b3c1c78d1ef17c1
yoshikouki <yoshikouki@gmail.com>
2021-09-05 00:12:36 +0900

    dividends.#{type}_date のカラムを作成。こちらに移行していく

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_134616) do
+ActiveRecord::Schema.define(version: 2021_09_04_145831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,12 @@ ActiveRecord::Schema.define(version: 2021_09_04_134616) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "notified", default: false
     t.bigint "company_id"
+    t.date "ex_dividend_date", null: false
+    t.date "record_date"
+    t.date "payment_date"
+    t.date "declaration_date"
     t.index ["company_id"], name: "index_dividends_on_company_id"
+    t.index ["ex_dividend_date", "symbol"], name: "index_dividends_on_ex_dividend_date_and_symbol", unique: true
     t.index ["symbol"], name: "index_dividends_on_symbol"
   end
 
