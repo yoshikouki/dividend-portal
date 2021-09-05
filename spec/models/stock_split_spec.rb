abbff30458a81ebf4684b92b87e8489613c95477
yoshikouki <yoshikouki@gmail.com>
2021-09-05 18:44:19 +0900

    StockSplit に validationテストを追加

--- spec/models/stock_split_spec.rb
+++ spec/models/stock_split_spec.rb
@@ -3,5 +3,15 @@
 require "rails_helper"
 
 RSpec.describe StockSplit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it "is valid" do
+      stock_split = StockSplit.new(
+        date: "2021-09-05",
+        symbol: "TEST",
+        numerator: 2,
+        denominator: 1,
+      )
+      expect(stock_split.valid?).to be true
+    end
+  end
 end
