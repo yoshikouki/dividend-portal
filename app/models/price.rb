35bde4f9e311877b0fd587f5ab12436f55d2f9c4
yoshikouki <yoshikouki@gmail.com>
2021-09-11 11:12:43 +0900

    株価が基準日の週に含まれるか判定する

--- app/models/price.rb
+++ app/models/price.rb
@@ -19,4 +19,17 @@ class Price
   attribute :label, :string
   attribute :change_over_time, :float
   attribute :symbol, :string
+
+  def for_the_week_of?(arg)
+    reference_date = to_date(arg)
+    date.between? reference_date.at_beginning_of_week, reference_date.at_end_of_week
+  end
+
+  private
+
+  def to_date(arg)
+    return arg if arg.is_a?(DateAndTime)
+
+    Date.parse(arg)
+  end
 end
---
+++ spec/models/price_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Price" do
+  describe "#for_the_week_of? 引数の Date が属する週の Price かどうかを判定する" do
+    context "引数の週に含まれる場合" do
+      it "true を返す" do
+        price = Price.new(date: "2021-09-07")
+        expect(price.for_the_week_of?("2021-09-05")).to be false # 先週の日曜日
+        expect(price.for_the_week_of?("2021-09-06")).to be true # 今週の月曜日
+        expect(price.for_the_week_of?("2021-09-12")).to be true # 今週の日曜日
+        expect(price.for_the_week_of?("2021-09-13")).to be false # 来週の月曜日
+      end
+    end
+  end
+end
