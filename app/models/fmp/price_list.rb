a0a0d38336976d7c22a8315ede3562839761d417
yoshikouki <yoshikouki@gmail.com>
2021-09-11 10:14:33 +0900

    クラスメソッドを先に置いときたい

--- app/models/fmp/price_list.rb
+++ app/models/fmp/price_list.rb
@@ -23,20 +23,20 @@ module Fmp
       symbol: :symbol,
     }.freeze
 
-    def initialize(responses = [])
-      @responses = responses
-    end
-
-    def prices
-      @dividend_calendar
-    end
-
     def self.historical(*symbols, from: nil, to: nil, timeseries: nil, serietype: nil)
       price_list = new
       symbols.flatten.each_slice(5) do |up_to_5_symbols|
         price_list.responses << Fmp.historical_prices(up_to_5_symbols, from: from, to: to, timeseries: timeseries, serietype: serietype)
       end
       price_list
+    end
+
+    def initialize(responses = [])
+      @responses = responses
+    end
+
+    def prices
+      @dividend_calendar
     end
 
     def list
