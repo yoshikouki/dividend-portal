4865f8c94118c72581b95002a7e26b6ba22c7e88
yoshikouki <yoshikouki@gmail.com>
2021-09-11 20:50:53 +0900

    コンテンツ生成の流れ

--- app/models/tweet/content/dividend_aristocrats.rb
+++ app/models/tweet/content/dividend_aristocrats.rb
@@ -4,8 +4,13 @@ class Tweet
   class Content
     class DividendAristocrats < Tweet::Content
       def ranking_of_weekly_price_drop_rate(reference_date: Date.current)
+        ranking_of_weekly_price_drop = Price::Weekly.dividend_aristocrats_in_order_of_change_percent
+        prices_for_one_year = Price.retrieve_by_api(symbol: ranking_of_weekly_price_drop.first, from: Date.current.last_year)
+        assigns = {
+          ranking: ranking_of_weekly_price_drop[0..4],
+        }
         text = render(file_name: __method__, assigns: assigns)
-        image = Chart.new.ranking_of_weekly_price_drop_rate
+        image = Chart.new.line_chart_of_price(prices_for_one_year)
         [text, image]
       end
     end
