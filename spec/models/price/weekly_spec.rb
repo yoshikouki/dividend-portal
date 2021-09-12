ea552abeba06d60e1a828ea4ed5c05133a4ad65c
yoshikouki <yoshikouki@gmail.com>
2021-09-12 22:23:03 +0900

    動作確認のために落ちるテストを用意

---
+++ spec/models/price/weekly_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Price::Weekly" do
+  describe ".dividend_aristocrats_in_order_of_change_percent" do
+    before do
+      Rails.application.load_seed
+    end
+
+    it "true を返す" do
+      VCR.use_cassette("models/price/weekly/dividend_aristocrats_in_order_of_change_percent") do
+        weekly_prices = Price::Weekly.dividend_aristocrats_in_order_of_change_percent
+        expected = Price::Weekly.new
+        expect(weekly_prices).to eq(expected)
+      end
+    end
+  end
+end
