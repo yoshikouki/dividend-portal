e9bc84b4d234520388f84320d5a4c33ea3e37868
yoshikouki <yoshikouki@gmail.com>
2021-09-12 18:00:13 +0900

    週足の株価を取得する

--- app/models/price/weekly.rb
+++ app/models/price/weekly.rb
@@ -2,24 +2,25 @@
 
 class Price
   class Weekly
-    attr_accessor :price_history
-
-    def initialize(daily: nil)
-      @price_history = price_history
-    end
+    attr_reader :daily_prices
 
     def self.dividend_aristocrats_in_order_of_change_percent(from: Date.current.beginning_of_week)
-      [
-        {date: "2021-09-01",
-         open: 56.38,
-         high: 56.8,
-         low: 56.28,
-         close: 56.69,
-         volume: 9404637.0,
-         change: 0.31,
-         change_percent: 0.55,
-         symbol: "KO"},
-      ]
+      symbols = Company.dividend_aristocrats.pluck(:symbol)
+      prices = Price.retrieve_by_api(symbols: symbols, from: from)
+      weekly_prices = calculate_from_daily_prices(prices)
+      weekly_prices.ascending_order_of_change_percent
+    end
+
+    def self.calculate_from_daily_prices(daily_prices)
+      new(daily_prices: daily_prices)
+    end
+
+    def initialize(daily_prices: nil)
+      @daily_prices = daily_prices
+    end
+
+    def ascending_order_of_change_percent
+      self
     end
   end
 end
