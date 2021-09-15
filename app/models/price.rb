bb6c96ea2daba19e87e7e03b8ce9413796bcced0
yoshikouki <yoshikouki@gmail.com>
2021-09-15 23:05:44 +0900

    指定日の週の価格か判定するメソッド

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Price < ApplicationRecord
+  def for_the_week_of?(arg)
+    reference_date = to_date(arg)
+    date.between? reference_date.at_beginning_of_week, reference_date.at_end_of_week
+  end
+
+  private
+
+  def to_date(arg)
+    arg.is_a?(DateAndTime) ? arg : Date.parse(arg)
+  end
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -3,5 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#for_the_week_of? 引数の Date が属する週の Price かどうかを判定する" do
+    context "引数の週に含まれる場合" do
+      it "true を返す" do
+        price = Price.create!(date: "2021-09-07")
+        expect(price.for_the_week_of?("2021-09-05")).to be false # 先週の日曜日
+        expect(price.for_the_week_of?("2021-09-06")).to be true # 今週の月曜日
+        expect(price.for_the_week_of?("2021-09-12")).to be true # 今週の日曜日
+        expect(price.for_the_week_of?("2021-09-13")).to be false # 来週の月曜日
+      end
+    end
+  end
 end
