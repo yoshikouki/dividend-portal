5951d2d499644819dd153c4264382e4d8e43371e
yoshikouki <yoshikouki@gmail.com>
2021-09-25 10:37:12 +0900

    APIからの取得をテストに反映

--- spec/models/tweet/content/dividend_aristocrats_spec.rb
+++ spec/models/tweet/content/dividend_aristocrats_spec.rb
@@ -7,7 +7,7 @@ describe "Tweet::Content::DividendAristocrats" do
     let!(:reference_date) { Date.new(2021, 9, 4) } # 土曜日
 
     before do
-      VCR.use_cassette "models/tweet/content/dividend_aristocrats/ranking_of_weekly_price_drop_rate" do
+      VCR.use_cassette("models/refresh/dividend_aristocrat/prices") do
         Refresh::DividendAristocrat.weekly_prices(reference_date: reference_date)
       end
     end
@@ -17,20 +17,22 @@ describe "Tweet::Content::DividendAristocrats" do
         <<~TWEET
           【配当貴族の週足値下がりランキング 2021-09-04 #米国株】
 
-          1. HRL (-2.83%)
-          2. BDX (-2.40%)
-          3. ADM (-2.27%)
-          4. CAH (-1.56%)
-          5. WBA (-1.55%)
+          1. ABBV (-6.86%)
+          2. NUE (-5.83%)
+          3. HRL (-4.55%)
+          4. PPG (-4.43%)
+          5. LEG (-3.83%)
 
           ↓ 今週最も値下がりした銘柄の週足ラインチャート
         TWEET
       end
 
       it "週足値下がりランキングのコンテンツとチャート画像のパスを配列で返す" do
-        text, chart_path = Tweet::Content::DividendAristocrats.new.ranking_of_weekly_price_drop_rate(reference_date: reference_date)
-        expect(text).to eq expected_content
-        expect(chart_path).to eq "prices_chart_path"
+        VCR.use_cassette "models/tweet/content/dividend_aristocrats/ranking_of_weekly_price_drop_rate" do
+          text, chart_path = Tweet::Content::DividendAristocrats.new.ranking_of_weekly_price_drop_rate(reference_date: reference_date)
+          expect(text).to eq expected_content
+          expect(chart_path).to eq "prices_chart_path"
+        end
       end
     end
   end
