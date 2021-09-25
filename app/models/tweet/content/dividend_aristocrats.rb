327697bb704015643112dfd5fc9cff2e6d9ace16
yoshikouki <yoshikouki@gmail.com>
2021-09-25 10:18:58 +0900

    API経由でPriceを取得

--- app/models/tweet/content/dividend_aristocrats.rb
+++ app/models/tweet/content/dividend_aristocrats.rb
@@ -5,7 +5,7 @@ class Tweet
     class DividendAristocrats < Tweet::Content
       def ranking_of_weekly_price_drop_rate(reference_date: Date.current)
         ranking_of_weekly_price_drop = WeeklyPrice.dividend_aristocrats_sorted_by_change_percent(reference_date).slice(0, 5)
-        prices_for_one_year = Price.where(symbol: ranking_of_weekly_price_drop[0].symbol, date: reference_date.last_year..reference_date)
+        prices_for_one_year = Price.where_from_api(symbol: ranking_of_weekly_price_drop[0].symbol, date: reference_date.last_year..reference_date)
         assigns = {
           ranking: ranking_of_weekly_price_drop,
         }
