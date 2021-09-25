0524c78c92f38578198f204b6a0021c8b8ca42c0
yoshikouki <yoshikouki@gmail.com>
2021-09-25 23:21:15 +0900

    チャートの微調整

--- app/models/quick_chart_wrapper/config.rb
+++ app/models/quick_chart_wrapper/config.rb
@@ -75,6 +75,82 @@ class QuickChartWrapper
       }
     CONFIG
 
+    LINE_CHART_OF_PRICE = <<~LINE_CHART_OF_PRICE
+      {
+          type: "line",
+          data: {
+            labels: %<labels>s,
+            datasets: [
+              {
+                type: "line",
+                label: "%<line_label>s",
+                data: %<line_data>s,
+                borderColor: "rgba(75, 192, 192, 0.3)",
+                backgroundColor: getGradientFillHelper("vertical", [ 
+                  "#a336eb", 
+                  "#36a2eb", 
+                ]),
+                pointRadius: 0,
+                yAxisID: "left",
+              },
+              {
+                type: "bar",
+                label: "%<bar_label>s",
+                data: %<bar_data>s,
+                barPercentage: 1,
+                categoryPercentage: 1,
+                backgroundColor:"rgba(0,0,0, 0.15)",
+                yAxisID: "right",
+              },
+            ]
+          },
+          options: {
+            scales: {
+              xAxes: [
+                {
+                  gridLines: { display: false },
+                  ticks: { fontSize: 20, fontFamily: "MONO", fontStyle: "bold", autoSkipPadding: 30,
+                          callback: (val, index) => {
+                            let date = new Date(val.toLocaleString());
+                            return (date.getMonth() + 1) + "月-" + date.getFullYear();
+                          } },
+                },
+              ],
+              yAxes: [
+                {
+                  id: "left",
+                  position: "left",
+                  ticks: { fontSize: #{FONT_SIZE}, fontFamily: "#{FONT_FAMILY}", fontStyle: "bold",
+                          callback: (val) => {
+                            let price = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 0 });
+                            return price.format(val.toLocaleString());
+                          } },
+                  gridLines: { display: false },
+                },
+                {
+                  id: "right",
+                  position: "right",
+                  ticks: { fontSize: #{FONT_SIZE}, fontFamily: "#{FONT_FAMILY}", fontStyle: "bold",
+                          callback: (val) => {
+                            return val.toLocaleString() + "%%";
+                          } },
+                },
+              ]
+            },
+            legend: {
+              position: "bottom",
+              labels: { fontSize: #{FONT_SIZE}, fontFamily: "#{FONT_FAMILY}", fontStyle: "bold" }
+            },
+            title: {
+              text: "%<title>s",
+              display: true,
+              fontSize: #{FONT_SIZE * 2},
+              fontFamily: "#{FONT_FAMILY}"
+            }
+          }
+        }
+    LINE_CHART_OF_PRICE
+
     class << self
       def new_dividend_of_dividend_aristocrats(**arg)
         format(
@@ -92,7 +168,7 @@ class QuickChartWrapper
 
       def line_chart_of_price(**arg)
         format(
-          NEW_DIVIDEND_OF_DIVIDEND_ARISTOCRATS,
+          LINE_CHART_OF_PRICE,
           {
             title: arg[:title],
             labels: arg[:labels],
